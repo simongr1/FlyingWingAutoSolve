64bf27447e0f39e50d4ab4d9d41273905f9ecbde
st177427 <st177427@stud.uni-stuttgart.de>
2025-01-25 17:32:08 +0100

    checkpolar is now ready

--- Tools/checkpolar.py
+++ Tools/checkpolar.py
@@ -8,12 +8,12 @@ import sys
 #import dependecies
 dependencies_dir= os.path.abspath(os.path.join(os.getcwd(),"../WingSolver_dependencies"))
 sys.path.append(dependencies_dir)
-import WingSolver_dependencies.WingSolver_Dependencies as wd
+import WingSolver_Dependencies as wd
 ################Information##################
 """You can change the PolyKey and the reference Key to switch between differnet results like LiftForce, DragForce, PitchTorque, RollTorque, YawTorque.
 If you want to plot anything else iteration by iteration you have to change the 4 Variables in Input (e.g to cA and cD)"""
 
-path="/home/simongr/Downloads/Archive/20250117_results/results"
+path="/home/simongr/Downloads/Archive/20250120_results/results"
 PolyKey= "PitchTorque"
 ReferenceKey= "alpha"
 
@@ -27,8 +27,7 @@ length=len(parameter["RootIncidence"])
 PolyReference=results[ReferenceKey]
 CurrentAllPoints= results["data"][PolyKey+"Raw"]
 PointsReference = results["data"][ReferenceKey+"Raw"]"""
-#the poly using the real ca
-CurrentPolystrue= [ wd.drag_curve(results["data"]["cL"][i],results["data"]["cD"][i]) for i in range(0,length)]
+
 #the points from the ca/cd approx
 density=1
 velocity=10
@@ -36,25 +35,43 @@ try:
 	reference_area=parameter["WingArea"]
 except:
 	reference_area= [parameter["RootChord"][i]*(parameter["TaperRatio"][i]+1)*parameter["WingLength"][i] for i in range(0,length)]
-CurrentAllPointscA= [wd.total_to_coefficient(results["data"]["LiftForceRaw"][i], density, velocity, reference_area[i]*1e-6) for i in range(0,length)]
-CurrentAllPointscD= [wd.total_to_coefficient(results["data"]["DragForceRaw"][i], density, velocity, reference_area[i]*1e-6) for i in range(0,length)]
-CurrentPolysApprox=results["data"]["DragPolar"]
+
+#the poly using the real ca
+DragPolartrue= [ wd.drag_curve(results["data"]["cL"][i],results["data"]["cD"][i]) for i in range(0,length)]
+True_cD=results["data"]["cD"]
+True_cL=results["data"]["cL"]
+
+CurrentAllPointscA_calc= [wd.total_to_coefficient(results["data"]["LiftForceRaw"][i], density, velocity, reference_area[i]*1e-6) for i in range(0,length)]
+CurrentAllPointscD_calc= [wd.total_to_coefficient(results["data"]["DragForceRaw"][i], density, velocity, reference_area[i]*1e-6) for i in range(0,length)]
+DragPolarApprox=results["data"]["DragPolar"]
+
 PolyReference=np.linspace(-1.5,1.5,100)
-CurrentAllPoints=results["data"]["cD"]
-PointsReference=results["data"]["cL"]
 
-datasets={}
-for i, poly in enumerate(CurrentPolysApprox):
+
+datasets_approx={}
+for i, poly in enumerate(DragPolarApprox):
     values=wd.calculate_function(PolyReference,poly)
-    truevalues= wd.calculate_function(PolyReference,CurrentPolystrue[i])
-    points=np.array(CurrentAllPoints[i])
-    datasets[i]=[points,values,truevalues,CurrentAllPointscA[i],CurrentAllPointscD[i]] #############################the points are saved in 0 and the approximation is svaed under 1
+    points_x=np.array(CurrentAllPointscA_calc[i])
+    points_y= -1* np.array(CurrentAllPointscD_calc[i])
+    points= [points_x,points_y]
+    bestcL,bestcD=wd.cL_cD_bestendurance(poly)
+    point_best=[bestcL,bestcD]
+    datasets_approx[i]=[points,values,point_best] #############################the points are saved in 0 and the approximation is svaed under 1
 
+datasets_Noapprox={}
+for i, poly in enumerate(DragPolartrue):
+    values=wd.calculate_function(PolyReference,poly)
+    points_x=np.array(True_cL[i])
+    points_y= np.array(True_cD[i])
+    points= [points_x,points_y]
+    bestcL,bestcD=wd.cL_cD_bestendurance(poly)
+    point_best=[bestcL,bestcD]
+    datasets_Noapprox[i]=[points,values,point_best] #############################the points are saved in 0 and the approximation is svaed under 1
 # Determine the global y-axis range for consistent scaling
-datasetValues=datasets.values()
-y_min = min(min(data[0]) for data in datasetValues)
+datasetValues=datasets_approx.values()
+"""y_min = min(min(data[0]) for data in datasetValues)
 y_max = max(max(data[0]) for data in datasetValues)
-
+"""
 
 # Initialize variables
 current_index_iteration = 0
@@ -62,11 +79,13 @@ current_index_dataset = 0
 
 # Create a figure and axis
 fig, ax = plt.subplots()
-line, = ax.plot(PolyReference,datasets[current_index_iteration][1], label=f"Approximation with wingArea")
-line2, = ax.plot(PolyReference,datasets[current_index_iteration][2],"g", label=f"Approximation cA/cD")
-points, = ax.plot(PointsReference[current_index_iteration],datasets[current_index_iteration][0],"go", label=f"Dataset(from paraview)")
-points2, = ax.plot(datasets[current_index_iteration][3],-1*np.array(datasets[current_index_iteration][4]),"bo", label=f"Dataset (from analyse result) ")
-hline= ax.axvline(x=1 , ls="--", c="r")
+line_approx, = ax.plot(PolyReference,datasets_approx[current_index_iteration][1], label=f"Approximation with wingArea", c="g")
+points_approx, = ax.plot(datasets_approx[current_index_iteration][0][0],datasets_approx[current_index_iteration][0][1],"go", label=f"Dataset(approx with wingarea")
+best_approx, = ax.plot(datasets_approx[current_index_iteration][2][0],datasets_approx[current_index_iteration][2][1],"ro", label=f"best endurance(approx with wingarea")
+
+line_true, = ax.plot(PolyReference,datasets_Noapprox[current_index_iteration][1], label=f"Approximation with paraview", c="b")
+points_true, = ax.plot(datasets_Noapprox[current_index_iteration][0][0],datasets_Noapprox[current_index_iteration][0][1],"bo", label=f"Dataset(approx with paraview")
+best_true, = ax.plot(datasets_Noapprox[current_index_iteration][2][0],datasets_Noapprox[current_index_iteration][2][1],"rx", label=f"best endurance(approx with paraview")
 ax.legend()
 ax.set_title(f"Plot Iteration: {current_index_iteration + 1} for {PolyKey}")
 ax.axhline(0, color='black', linewidth=0.8)  # Horizontal axis
@@ -77,15 +96,19 @@ ax.grid(True)
 
 # Set consistent axis limits
 ax.set_xlim(-1.5, 1.5)  # Assuming all datasets have the same length
-ax.set_ylim(-1, 3)
+ax.set_ylim(-0.05, 0.5)
 
 # Define a function to update the plot
 def update_plot(index):
-    line.set_ydata(datasets[index][1])  # Update the line data
-    line2.set_ydata(datasets[index][2])
-    points.set_ydata(datasets[index][0])
-    points2.set_data(datasets[index][3],-1* np.array(datasets[index][4]))
-    hline.set_xdata(index)
+    line_approx.set_ydata(datasets_approx[index][1])  # Update the line data
+    points_approx.set_data(datasets_approx[index][0])
+    best_approx.set_data([datasets_approx[index][2][0]],[datasets_approx[index][2][1]])
+    line_true.set_ydata(datasets_Noapprox[index][1])  # Update the line data
+    points_true.set_data(datasets_Noapprox[index][0])
+    best_true.set_data([datasets_Noapprox[index][2][0]],[datasets_Noapprox[index][2][1]])
+    #line2.set_ydata(datasets_approx[index][2])
+    #points2.set_data(datasets_approx[index][3],-1* np.array(datasets_approx[index][4]))
+    #hline.set_xdata([index])
     ax.set_title(f"Plot Iteration: {current_index_iteration + 1} for {PolyKey}")
     fig.canvas.draw()
 
@@ -94,9 +117,9 @@ def update_plot(index):
 def on_key(event):
     global current_index_iteration
     if event.key == "right":
-        current_index_iteration = (current_index_iteration + 1) % len(datasets)  # Move to the next dataset
+        current_index_iteration = (current_index_iteration + 1) % len(datasets_approx)  # Move to the next dataset
     elif event.key == "left":
-        current_index_iteration = (current_index_iteration - 1) % len(datasets)  # Move to the previous dataset
+        current_index_iteration = (current_index_iteration - 1) % len(datasets_approx)  # Move to the previous dataset
     elif event.key == "up":
         pass
     elif event.key == "down":
