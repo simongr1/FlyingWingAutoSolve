42a59b1a1582afbcd34efdd7325fbd1c3aff1608
Simon Grimm <st177427@stud.uni-stuttgart.de>
2025-02-26 09:17:10 +0100

    version: smaller aircraft with velocity controll(oszillating)

--- WingSolver_dependencies/costfunction.py
+++ WingSolver_dependencies/costfunction.py
@@ -3,7 +3,7 @@ def lift_cost(v_sqrd,v_old,lift_weight, L0,mass):
     return lift_weight*(-1/(0.01* mass*9.81)*(L0*v_sqrd/(v_old**2) - mass*9.81))**2
 def trim_cost(v_sqrd,v_old,trim_weight,M0):
     #set v_sqrd and v_old to one if you just wnat to calculate the current cost
-    return trim_weight*(2*M0*v_sqrd/(v_old**2))**2
+    return trim_weight*(M0*v_sqrd/(v_old**2))**2
 def drag_cost(v_sqrd,v_old,drag_weight,D0):
     #set v_sqrd and v_old to one if you just wnat to calculate the current cost
     return drag_weight*(3*D0*v_sqrd/(v_old**2))**2
